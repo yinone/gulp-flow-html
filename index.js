dbc6b923aee6e90ff80846326a272da28a8052a8
eleven <yinyi@taolx.com>
2016-10-31 22:12:54 +0800

    add test

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /*
-* @Author: eleven
-* @E-mail: eleven.image@gmail.com
-* @Date:   2016-10-31 17:13:50
-* @Last Modified by:   eleven
-* @Last Modified time: 2016-10-31 17:14:31
-*/
+ * @Author: eleven
+ * @E-mail: eleven.image@gmail.com
+ * @Date:   2016-10-31 17:13:50
+ * @Last Modified by:   eleven
+ * @Last Modified time: 2016-10-31 22:10:26
+ */
 
 'use strict';
 
@@ -13,3 +13,37 @@ const gutil = require('gulp-util');
 const PluginError = gutil.PluginError;
 const path = require('path');
 const PLUGIN_NAME = 'gulp-flow-url';
+
+function generator(content) {
+
+    if (!content) {
+        throw new PluginError(PLUGIN_NAME, 'Missing content!');
+    }
+
+    return through.obj(function(file, enc, cb) {
+
+        if (file.isNull()) {
+            cb(null, file);
+        }
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+            return cb();
+        }
+        if (file.isBuffer()) {
+
+            let modname = path.resolve(process.cwd());
+            let mainPath = path.dirname(file.path);
+            let basePath = mainPath.replace(modname, '');
+
+            file.contents = new Buffer(content);
+            console.dir(file)
+            file.extname = 'html';
+            // file.contens = html;
+        }
+
+        cb(null, file);
+
+    });
+}
+
+module.exports = generator
--- test/test.js
+++ test/test.js
@@ -3,13 +3,38 @@
 * @E-mail: eleven.image@gmail.com
 * @Date:   2016-10-31 17:16:04
 * @Last Modified by:   eleven
-* @Last Modified time: 2016-10-31 17:16:27
+* @Last Modified time: 2016-10-31 22:04:06
 */
-
-'use strict';
 
 'use strict';
 
 let assert = require('assert');
 let File = require('vinyl');
-let flowUrl = require('../');
+let flowHTML = require('../');
+
+describe('gulp-flow-html', function() {
+	describe('in buffer mode', function() {
+		it('append content', function(done) {
+
+			let flowhtml = flowHTML('@@include');
+
+			let file = new File({
+				cwd: '/',
+  				base: '/test/',
+  				path: '/test/file.js',
+				contents: new Buffer('test')
+			});
+
+			flowhtml.write(file);
+
+			flowhtml.once('data', function(file) {
+
+				console.log(file.contents.toString('utf8'))
+				assert(file.isBuffer());
+				assert.equal(file.contents.toString('utf8'), '@@include');
+				done();
+			
+			})
+		})
+	})
+})
